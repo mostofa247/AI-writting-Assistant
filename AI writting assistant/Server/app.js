66d8dae3aa5a41e1d6489ecf1127976d45b0d918
unknown <mdshourov5596@gmail.com>
2025-06-23 14:37:25 +0600

    add client side and complete project

--- AI writting assistant/Server/app.js
+++ AI writting assistant/Server/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const analyzeRouter = require("./routes/analyze");
 const grammarCheck = require("./routes/grammarChecker");
 const spellChecker = require("./routes/spellChecker");
-require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
